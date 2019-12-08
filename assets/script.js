4a4d76a4bfa73c48d8e1468fc10c8f1b3ea67e03
Brandon Downhour <xtremeelement@gmail.com>
2019-12-08 18:34:49 -0500

    created essential files

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+var data = {
+  stuff: [
+    {
+      question: "What",
+      answers: ["1", "2", "3", "4"],
+      answer: 1
+    },
+    {
+      question: "What",
+      answers: ["1", "2", "3", "4"],
+      answer: 2
+    },
+    {
+      question: "What",
+      answers: ["1", "2", "3", "4"],
+      answer: 3
+    },
+    {
+      question: "What",
+      answers: ["1", "2", "3", "4"],
+      answer: 4
+    }
+  ]
+};
+
+let 
+
+console.log(data);
