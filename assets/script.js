4f213e001f771247eb4dea07d9df5d274231d294
xtremeelement <xtremeelement@gmail.com>
2019-12-08 20:27:57 -0500

    almost completed logic

--- assets/script.js
+++ assets/script.js
@@ -1,48 +1,101 @@
-var data = {
-  stuff: [
-    {
-      question: "What",
-      answers: ["1", "2", "3", "4"],
-      answer: 1
-    },
-    {
-      question: "What2",
-      answers: ["1", "2", "3", "4"],
-      answer: 2
-    },
-    {
-      question: "What3",
-      answers: ["1", "2", "3", "4"],
-      answer: 3
-    },
-    {
-      question: "What4",
-      answers: ["1", "2", "3", "4"],
-      answer: 4
-    }
-  ]
-};
+const questions = [
+  {
+    question: "What is 1",
+    answers: ["what", "what2", "what3", "what4"],
+    key: 1
+  },
+  {
+    question: "What is 2",
+    answers: ["what", "what2", "what3", "what4"],
+    key: 1
+  },
+  {
+    question: "What is 3",
+    answers: ["what", "what2", "what3", "what4"],
+    key: 1
+  },
+  {
+    question: "What is 4",
+    answers: ["what", "what2", "what3", "what4"],
+    key: 1
+  }
+];
 
 let header = document.querySelector("#header");
 let start = document.querySelector(".beginQuiz");
+let next = document.querySelector("#next");
+let result = document.querySelector(".result");
 let index = 0;
+let time = 59;
+let score = 0;
+let allScores = [];
+var highScores = localStorage.getItem("highscores");
+let timer = document.querySelector("#timer");
+let quizGame = document.querySelector("#quizGame");
+let scorediv = document.querySelector("#highscores");
+next.addEventListener("click", nextQuestion);
+
 function startQuiz() {
+  timer.style = "display: flex;";
   start.style = "display: none;";
-  header.innerHTML = data.stuff[index].question;
+  next.style = "display: flex;";
+  timeThing;
+  header.innerHTML = questions[index].question;
   for (var i = 0; i < 4; i++) {
     let question = document.getElementById(`${i}`);
-    question.innerHTML = data.stuff[index].answers[i];
+    question.innerHTML = questions[index].answers[i];
   }
+}
+var timeThing = setInterval(function() {
+  timer.innerHTML = time;
+  time--;
+  if (time < 0) {
+    clearInterval(timeThing);
+    result.innerHTML = "Times Up!";
+  }
+}, 1000);
 
-  document.addEventListener("click", function() {
-    event.stopPropagation();
-    if ((this.value = data.stuff[index].answer)) {
-      console.log("yes");
-    } else {
-      console.log("no");
-    }
-  });
+function checkAnswer() {
+  var target = event.currentTarget;
   index++;
+  console.log(index);
+  if (index == 4) {
+    console.log("hello");
+    endGame();
+  }
+  if (target.id == questions[index].key) {
+    result.innerHTML = "Correct!";
+    score += time;
+    nextQuestion();
+    console.log(score);
+    console.log("correct");
+  } else {
+    result.innerHTML = "Incorrect!";
+    time -= 10;
+    console.log("incorrect");
+    nextQuestion();
+  }
 }
 
+function nextQuestion() {
+  header.innerHTML = questions[index].question;
+  for (var i = 0; i < 4; i++) {
+    let question = document.getElementById(`${i}`);
+    question.innerHTML = questions[index].answers[i];
+  }
+}
+
+function endGame() {
+  console.log("im in");
+  time = 0;
+  timer.innerHTML = "High Scores";
+  clearInterval(timeThing);
+  quizGame.style = "display: none;";
+  scorediv.style = "display: flex";
+  let temp = { score };
+  allScores.push(temp);
+  localStorage.setItem("highScores", JSON.stringify(allScores));
+  var publishScores = JSON.parse(localStorage.getItem("highScores"));
+  console.log(publishScores);
+}
 // console.log(data);
