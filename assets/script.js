180993da037b9d69e7a0775961835cc06ea46c0d
Brandon Downhour <xtremeelement@gmail.com>
2019-12-08 19:01:04 -0500

    started building app

--- assets/script.js
+++ assets/script.js
@@ -6,23 +6,43 @@ var data = {
       answer: 1
     },
     {
-      question: "What",
+      question: "What2",
       answers: ["1", "2", "3", "4"],
       answer: 2
     },
     {
-      question: "What",
+      question: "What3",
       answers: ["1", "2", "3", "4"],
       answer: 3
     },
     {
-      question: "What",
+      question: "What4",
       answers: ["1", "2", "3", "4"],
       answer: 4
     }
   ]
 };
 
-let 
+let header = document.querySelector("#header");
+let start = document.querySelector(".beginQuiz");
+let index = 0;
+function startQuiz() {
+  start.style = "display: none;";
+  header.innerHTML = data.stuff[index].question;
+  for (var i = 0; i < 4; i++) {
+    let question = document.getElementById(`${i}`);
+    question.innerHTML = data.stuff[index].answers[i];
+  }
 
-console.log(data);
+  document.addEventListener("click", function() {
+    event.stopPropagation();
+    if ((this.value = data.stuff[index].answer)) {
+      console.log("yes");
+    } else {
+      console.log("no");
+    }
+  });
+  index++;
+}
+
+// console.log(data);
